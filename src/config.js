19b76a1d80b6419a44459d0c139d81cb6c595072
AbbasPithawala <abbaspithawala098@gmail.com>
2025-01-21 14:25:28 +0530

    Creating a new repo and first commit

---
+++ src/config.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+export const axiosInstance = axios.create({
+    baseURL : "http://localhost:4545/"
+    // baseURL : "http://52.195.10.133/"
+    // baseURL : "http://35.76.110.17/"
+})
+
+export const axiosInstance2 = axios.create({
+    baseURL : "http://localhost:4545/"
+    // baseURL : "http://52.195.10.133/"
+    // baseURL : "http://35.76.110.17/"
+})
