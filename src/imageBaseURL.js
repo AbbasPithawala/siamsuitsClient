2c24bd719d5d9ba9be052fe800650f720bfa04e9
AbbasPithawala <abbaspithawala098@gmail.com>
2025-01-25 16:03:43 +0530

    changed

--- src/imageBaseURL.js
+++ src/imageBaseURL.js
@@ -2,5 +2,5 @@
 export const PicBaseUrl = "https://siamsuitsimages.s3.ap-northeast-1.amazonaws.com/images/";
 export const PicBaseUrl2 = "https://res.cloudinary.com/di5etqzhu/image/upload/v1666081152/";
 export const PicBaseUrl4 = "https://siamsuitsimages.s3.ap-northeast-1.amazonaws.com/"
-export const PicBaseUrl3 = "http://localhost:4545/";
-// export const PicBaseUrl3 = "http://52.195.10.133/";
+// export const PicBaseUrl3 = "http://localhost:4545/";
+export const PicBaseUrl3 = "http://13.231.215.153/";
